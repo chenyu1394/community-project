d3b67429fb2762680d05466c982a7f168836956b
chenyu1394 <1394777162@qq.com>
2019-11-17 21:44:42 +0800

    update

---
+++ community-oauth2/src/main/java/com/chenyu/community/oauth2/controller/TbUserController.java
@@ -0,0 +1,15 @@
+package com.chenyu.community.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class TbUserController {
+    //获取用户信息
+    @RequestMapping("/user")
+    public Object currentUserName(Principal principal) {
+        return principal;
+    }
+}
